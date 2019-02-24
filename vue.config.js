f0ace2238caaa2b5601597be69c11226165f6d3e
DemEvo <urainmq@gmail.com>
2019-02-24 16:49:55 +0100

    bootstrap vue plagin add

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    assetsDir: 'static',
+    css: {
+        sourceMap: true,
+    },
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'https://kasta.ua',
+                changeOrigin: true,
+            },
+        },
+        port: 8020,
+    },
+
+};
